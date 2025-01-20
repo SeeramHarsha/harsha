fc3bf1ce45d851257adfb482e036c97abf31fd7a
Seeram Harshavardhan <66600388+SeeramHarsha@users.noreply.github.com>
2025-01-20 13:34:00 +0530

    Update college_details.py

--- college_details.py
+++ college_details.py
@@ -1,12 +1,2 @@
-[
-  {
-    "name": "ABC Engineering College",
-    "location": "Hyderabad",
-    "courses": ["CSE", "ECE", "Mechanical"]
-  },
-  {
-    "name": "XYZ College of Arts",
-    "location": "Chennai",
-    "courses": ["History", "Economics", "English"]
-  }
-]
+use college_db
+db.college_details.insertOne({"name": "Test College", "location": "Test City"})
