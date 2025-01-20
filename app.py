af10cbd0bd71b49d3b4ee56ebfb2e8018459f276
Seeram Harshavardhan <66600388+SeeramHarsha@users.noreply.github.com>
2025-01-20 13:22:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Connect to MongoDB
-# Replace '<username>', '<password>', and '<cluster-url>' with your MongoDB connection details
-print("hello")
-client = MongoClient("mongodb://localhost:27017/college_db")
+client = MongoClient("mongodb://localhost:27017/")
 db = client["college_db"]
 college_collection = db["college_details"]
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to the Flask App!"
 
 @app.route("/colleges", methods=["GET"])
 def get_colleges():
     # Retrieve all college details from the database
-    colleges = list(college_collection.find({}, {"_id": 0}))  # Exclude MongoDB's '_id' field
+    colleges = list(college_collection.find({}, {"_id": 0}))
     return jsonify({"colleges": colleges})
 
 if __name__ == "__main__":
