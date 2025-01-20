0a1c43df66fa86601bb5a3bd66ce1865074d708d
Seeram Harshavardhan <66600388+SeeramHarsha@users.noreply.github.com>
2025-01-20 12:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ def get_colleges():
     colleges = list(college_collection.find({}, {"_id": 0}))  # Exclude MongoDB's '_id' field
     return jsonify({"colleges": colleges})
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
